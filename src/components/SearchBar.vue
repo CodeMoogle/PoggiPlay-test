21fd45ef1c7b7775c8ea939d98cbaa1037651e0d
CodeMoogle <codemoogle90@gmail.com>
2021-01-21 21:53:10 +0300

    SearchBar component added. README updated.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-text-field
+		class="pt-2 pb-2"
+		v-model="searchTitle"
+		@keyup.enter="getSearchQuery"
+		@click:append="getSearchQuery"
+		label="Search a package"
+		hint="For example, jquery or vue-carousel"
+		append-icon="mdi-magnify"
+		outlined
+		clearable
+	></v-text-field>
+</template>
+
+<script>
+export default {
+	name: "SearchBar",
+	data() {
+		return {
+			searchTitle: "",
+		};
+	},
+	methods: {
+		getSearchQuery() {
+			/* make query */
+			this.searchTitle = "";
+		},
+	},
+};
+</script>
